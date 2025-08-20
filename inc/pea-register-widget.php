1cafccd69464b6004dda9a5b2fcfe0b859c90ef6
Risad212 <hafez.risad@gmail.com>
2025-08-20 22:17:57 +0600

    started image widget

--- inc/pea-register-widget.php
+++ inc/pea-register-widget.php
@@ -3,9 +3,10 @@
 function pea_register_widget( $widgets_manager ) {
 
 	require_once dirname(__DIR__). '/widgets/heading.php';
-    
+    require_once dirname(__DIR__). '/widgets/image.php';
 	
 	$widgets_manager->register( new \PEA_Hedding() );
+	$widgets_manager->register( new \PEA_Image() );
 
 }
 add_action( 'elementor/widgets/register', 'pea_register_widget' );
