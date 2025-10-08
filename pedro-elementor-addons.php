1757e4e879fe48968ac94a174f9996bb76d0d0a9
Risad212 <hafez.risad@gmail.com>
2025-10-08 21:39:54 +0600

    move main funcality code to another files name plugin and and create soem function to checking version php elemetnor and checking more before loading the plugin

--- pedro-elementor-addons.php
+++ pedro-elementor-addons.php
@@ -9,121 +9,188 @@
  * Domain Path: /languages
  */
 
-// Exit if accessed directly
 
+// Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
      exit;
 } 
 
 
+final class Pedro_Elementro_Addon {
 
-class Pedro_Elementor_Addons {
+	/**
+	 * Defince Plugin Version
+	 * 
+	 * @since 1.2.1
+	 * 
+	 * @var string
+	 */
+	const VERSION = '1.0.0';
 
-	
-	private static $_instance = null;
+	/**
+	 * Defince Minimum Elementor Version
+	 *
+	 * @since 1.0.0
+	 * 
+	 * @var string Minimum Elementor version required to run the plugin.
+	 */
+	const MINIMUM_ELEMENTOR_VERSION = '3.0.0';
 
-    /**
-     * instance of plugin
-     * 
-     * @since 1.0.0
-     * 
-     * @access public
-     */
-	public static function instance() {
-		if ( is_null( self::$_instance ) ) {
-			self::$_instance = new self();
+	/**
+	 * Defince Minimum PHP Version
+	 *
+	 * @since 1.0.0
+	 * 
+	 * @var string Minimum PHP version required to run the plugin.
+	 */
+	const MINIMUM_PHP_VERSION = '7.0';
+
+	/**
+	 * Constructor
+	 *
+	 * @since 1.0.0
+	 * 
+	 * @access public
+	 */
+	public function __construct() {
+		// Init Plugin
+		add_action( 'plugins_loaded', array( $this, 'plugin_init' ) );
+	}
+
+	/**
+	 * Initialize The Plugin
+	 *
+	 * Validates that Elementor is already loaded.
+	 * 
+	 * Checks for basic plugin requirements, if one check fail don't continue,
+	 * 
+	 * if all check have passed include the plugin class.
+	 *
+	 * Fired by plugins_loaded action hook.
+	 *
+	 * @since 1.2.0
+	 * 
+	 * @access public
+	 */
+	public function plugin_init() {
+
+		// Check if Elementor installed and activated
+		if ( ! did_action( 'elementor/loaded' ) ) {
+			add_action( 'admin_notices', array( $this, 'admin_notice_missing_main_plugin' ) );
+
+			return;
 		}
 
-		return self::$_instance;
-	}
-    
+		// Check for required Elementor version
+		if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
+			add_action( 'admin_notices', array( $this, 'admin_notice_minimum_elementor_version' ) );
 
-    public function define_constant(){
-       define('PEA_VERSION', '1.0.0');
+			return;
+		}
+
+		// Check for required PHP version
+		if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
+			add_action( 'admin_notices', array( $this, 'admin_notice_minimum_php_version' ) );
+
+			return;
+		}
+        
+		// defined constant for plugin
+		$this->define_constant();
+
+		// Once we get here, We have passed all validation checks so we can safely include our plugin
+		require_once( 'plugin.php' );
+	}
+
+	/**
+	 * Defince Constant
+	 * 
+	 * @since 1.0.0
+	 * 
+	 * @access public
+	 */
+	public function define_constant(){
        // URL (for enqueuing CSS/JS)
        define('PEA_PLUGIN_URL', plugins_url('/', __FILE__));
+
        // PATH (for including PHP files)
       define('PEA_PLUGIN_PATH', plugin_dir_path(__FILE__));
     }
 
-
-    /**
-     * Register Widgets
-     * 
-     * @since 1.0.0
-     * 
-     * @access public
-     */
-	public function register_widgets( $widgets_manager ) {
-		require_once PEA_PLUGIN_PATH . 'widgets/testimonial.php';
-
-		$widgets_manager->register( new \Pea_Testimonial() );
-	}
-
-	 /**
-     * Register Widgets categories
-     * 
-     * @since 1.0.0
-     * 
-     * @access public
-     */
-	public function register_widget_categories( $elements_manager ) {
-		$elements_manager->add_category(
-			'pedro',
-			[
-				'title' => __( 'Pedro', 'pedro-elementor-addons' ),
-			]
-		);
-	}
-
-	 /**
-     * Register Widgets categories
-     * 
-     * @since 1.0.0
-     * 
-     * @access public
-     */
-	public function enqueue_scripts() {
-		wp_enqueue_style( 'pea-swiper-css', PEA_PLUGIN_PATH . 'assets/css/pea-swiper-bundle.min.css', [], '1.0.0', 'all' );
-		wp_enqueue_style( 'pea-main-css',   PEA_PLUGIN_PATH . 'assets/css/pea-main.css', [], '1.0.0', 'all' );
-
-		wp_enqueue_script( 'pea-swiper-js', PEA_PLUGIN_PATH . 'assets/js/pea-swiper-bundle.min.js', [ 'jquery' ], '1.0.0', true );
-		wp_enqueue_script( 'pea-main-js',   PEA_PLUGIN_PATH . 'assets/js/pea-main.js', ['jquery'], '1.0.0', true );
-
-	}
-
-	// public function enqueue_editor_scripts(){
-	// 	wp_enqueue_script( 'reskill-elementor-editor', RCORE_URL . 'assets/js/editor.min.js', ['elementor-editor','jquery'], '1.0.0', true);
-	// }
 	/**
-	 *  Plugin class constructor
+	 * Admin notice
 	 *
-	 * Register plugin action hooks and filters
+	 * Warning when the site doesn't have Elementor installed or activated.
 	 *
-	 * @since 1.2.0
+	 * @since 1.0.0
+	 * 
 	 * @access public
 	 */
-	public function __construct() {
+	public function admin_notice_missing_main_plugin() {
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
 
-        // Define constant
-        $this->define_constant();
+		$message = sprintf(
+		/* translators: 1: Plugin name 2: Elementor */
+			__( '"%1$s" requires "%2$s" to be installed and activated.', 'pedro-elementor-addons' ),
+			'<strong>' . __( 'Pedro Elementor Addon', 'pedro-elementor-addons' ) . '</strong>',
+			'<strong>' . __( 'Elementor', 'pedro-elementor-addons' ) . '</strong>'
+		);
 
-		// Register widget scripts
-		// add_action( 'elementor/frontend/after_register_scripts', [ $this, 'widget_scripts' ] );
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+	}
 
-		// Register widgets
-		add_action( 'elementor/widgets/register', [ $this, 'register_widgets' ] );
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required Elementor version.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function admin_notice_minimum_elementor_version() {
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
 
-		// Register widget categories
-		add_action( 'elementor/elements/categories_registered', [ $this, 'register_widget_categories' ] );
+		$message = sprintf(
+		/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'pedro-elementor-addons' ),
+			'<strong>' . esc_html__( 'Pedro Elementor Addon', 'pedro-elementor-addons' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'pedro-elementor-addons' ) . '</strong>',
+			self::MINIMUM_ELEMENTOR_VERSION
+		);
 
-		// Enqueue scripts for widgets
-		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+	}
 
-		// // Enqueue scripts for Elementor Editor
-		// add_action( 'elementor/editor/after_enqueue_scripts', [$this, 'enqueue_editor_scripts']);
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required PHP version.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function admin_notice_minimum_php_version() {
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
+
+		$message = sprintf(
+		/* translators: 1: Plugin name 2: PHP 3: Required PHP version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'pedro-elementor-addons' ),
+			'<strong>' . esc_html__( 'Pedro Elementor Addon', 'pedro-elementor-addons' ) . '</strong>',
+			'<strong>' . esc_html__( 'PHP', 'pedro-elementor-addons' ) . '</strong>',
+			self::MINIMUM_PHP_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
 	}
 }
 
-// Instantiate Plugin Class
-Pedro_Elementor_Addons::instance();
+// Instantiate Parker_Core.
+new Pedro_Elementro_Addon();
+
+
---
+++ plugin.php
@@ -0,0 +1,111 @@
+<?php
+
+class Plugin {
+	
+     /**
+     * store instance of plugin
+     * 
+     * @since 1.0.0
+     * 
+     * @var null
+     */
+	private static $_instance = null;
+
+    /**
+     * instance of plugin
+     * 
+     * @since 1.0.0
+     * 
+     * @return instance
+     */
+	public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+
+		return self::$_instance;
+	}
+
+    /**
+     * Register Widgets
+     * 
+     * @since 1.0.0
+     * 
+     * @access public
+     */
+	public function register_widgets( $widgets_manager ) {
+		require_once PEA_PLUGIN_PATH . 'widgets/testimonial.php';
+		$widgets_manager->register( new \Pea_Testimonial() );
+	}
+
+	 /**
+     * Register Widgets categories
+     * 
+     * @since 1.0.0
+     * 
+     * @access public
+     */
+	public function register_widget_categories( $elements_manager ) {
+		$elements_manager->add_category(
+			'pedro',
+			[
+				'title' => __( 'Pedro', 'pedro-elementor-addons' ),
+			]
+		);
+	}
+
+	 /**
+     * Register Widgets categories
+     * 
+     * @since 1.0.0
+     * 
+     * @access public
+     */
+	public function enqueue_scripts() {
+		wp_enqueue_style( 'pea-swiper-css', PEA_PLUGIN_PATH . 'assets/css/pea-swiper-bundle.min.css', [], '1.0.0', 'all' );
+		wp_enqueue_style( 'pea-main-css',   PEA_PLUGIN_PATH . 'assets/css/pea-main.css', [], '1.0.0', 'all' );
+
+		wp_enqueue_script( 'pea-swiper-js', PEA_PLUGIN_PATH . 'assets/js/pea-swiper-bundle.min.js', [ 'jquery' ], '1.0.0', true );
+		wp_enqueue_script( 'pea-main-js',   PEA_PLUGIN_PATH . 'assets/js/pea-main.js', ['jquery'], '1.0.0', true );
+
+	}
+
+	/**
+	 * Elementor Editor Js Files
+	 * 
+	 * @since 1.0.0
+	 * 
+	 * @access public
+	 */
+	public function enqueue_editor_scripts(){
+		wp_enqueue_script( 'reskill-elementor-editor', PEA_PLUGIN_URL . 'assets/js/editor.min.js', ['elementor-editor','jquery'], '1.0.0', true);
+	}
+
+
+	/**
+	 *  Plugin class constructor
+	 * 
+	 * @since 1.0.0
+	 * 
+	 * @access public
+	 */
+	public function __construct() {
+
+		// Register widget scripts
+		// add_action( 'elementor/frontend/after_register_scripts', [ $this, 'widget_scripts' ] );
+
+		// Register widgets
+		add_action( 'elementor/widgets/register', [ $this, 'register_widgets' ] );
+
+		// Register widget categories
+		add_action( 'elementor/elements/categories_registered', [ $this, 'register_widget_categories' ] );
+
+		// Enqueue scripts for widgets
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
+
+		// // Enqueue scripts for Elementor Editor
+		add_action( 'elementor/editor/after_enqueue_scripts', [$this, 'enqueue_editor_scripts']);
+	}
+}
+
+Plugin::instance();
