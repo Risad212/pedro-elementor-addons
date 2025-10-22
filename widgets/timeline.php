49d83a1af0706ee04f0166e022f5eb4a3b1b83e2
Risad212 <hafez.risad@gmail.com>
2025-10-22 21:51:43 +0600

    done

--- widgets/timeline.php
+++ widgets/timeline.php
@@ -35,18 +35,65 @@ class Pea_Timeline extends Widget_Base {
       $this->start_controls_section(
 			'content_section',
 			[
-				'label' => esc_html__( 'Content', 'textdomain' ),
+				'label' => esc_html__( 'Content', 'pedro-elementor-icon' ),
 				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
 			]
 		);
 
+		
 		$this->add_control(
-			'widget_title',
+			'timeline_list',
 			[
-				'label' => esc_html__( 'Title', 'textdomain' ),
-				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => esc_html__( 'Default title', 'textdomain' ),
-				'placeholder' => esc_html__( 'Type your title here', 'textdomain' ),
+				'label' => esc_html__( 'Timeline', 'pedro-elementor-icon' ),
+				'type' => \Elementor\Controls_Manager::REPEATER,
+				'fields' => [
+					[
+						'name'        => 'icon',
+						'label'       => esc_html__( 'Icon', 'pedro-elementor-icon' ),
+						'type'        => Controls_Manager::ICONS,
+						'default'     => [
+					    'value'       => 'fas fa-circle',
+					    'library'     => 'fa-solid',
+				         ],
+						'label_block' => true,
+					],
+					[
+						'name'        => 'title',
+						'label'       => esc_html__( 'Title', 'pedro-elementor-icon' ),
+						'type'        => Controls_Manager::TEXT,
+						'default'     => esc_html__( 'Title' , 'pedro-elementor-icon' ),
+						'label_block' => true,
+					],
+					[
+						'name'        => 'discription',
+						'label'       => esc_html__( 'Discription', 'pedro-elementor-icon' ),
+						'type'        => Controls_Manager::TEXTAREA,
+						'default'     => esc_html__( 'Discription' , 'pedro-elementor-icon' ),
+						'label_block' => true,
+					],
+					[
+						'name'        => 'education_info',
+						'label'       => esc_html__( 'Education Info', 'pedro-elementor-icon' ),
+						'type'        => Controls_Manager::TEXTAREA,
+						'default'     => esc_html__( 'Computer Science' , 'pedro-elementor-icon' ),
+						'label_block' => true,
+					],
+				],
+				'default' => [
+					[
+						'list_title'   => esc_html__( 'Timeline #1', 'pedro-elementor-icon' ),
+						'list_content' => esc_html__( 'Item content. Click the edit button to change this text.', 'pedro-elementor-icon' ),
+					],
+					[
+						'list_title'   => esc_html__( 'Timeline #2', 'pedro-elementor-icon' ),
+						'list_content' => esc_html__( 'Item content. Click the edit button to change this text.', 'pedro-elementor-icon' ),
+					],
+					[
+						'list_title'   => esc_html__( 'Timeline #2', 'pedro-elementor-icon' ),
+						'list_content' => esc_html__( 'Item content. Click the edit button to change this text.', 'pedro-elementor-icon' ),
+					],
+				],
+				'title_field' => '{{{ list_title }}}',
 			]
 		);
 
@@ -55,7 +102,11 @@ class Pea_Timeline extends Widget_Base {
 
   protected function render(): void {
 		$settings = $this->get_settings_for_display();
+         
+		$timeline_list = $settings['timeline_list'];
 
+		var_dump($timeline_list);
+		
 		?>
 
 		<div class="pea-main-timeline">
@@ -68,30 +119,8 @@ class Pea_Timeline extends Widget_Base {
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit placeat tenetur, maiores, consectetur enim ullam quo earum laborum eius possimus alias repellendus illum!
             </p>
             </div>
-        </div>
-
-        <div class="pea-timeline">
-            <span class="pea-icon"><svg  version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  viewBox="1052 796 200 200" enable-background="new 1052 796 200 200" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M1151.998,921.75c-4.129,0-8.17-0.771-12.01-2.292l-50.167-19.888c0,11.08,0,27.65,0,32.066 c0,15.562,27.836,28.174,62.178,28.174s62.181-12.612,62.181-28.174v-32.067l-50.172,19.889 C1160.168,920.979,1156.127,921.75,1151.998,921.75z"></path> <path d="M1248.592,867.082l-87.989-34.878c-5.526-2.19-11.681-2.19-17.208,0l-87.988,34.878c-2.057,0.815-3.407,2.804-3.407,5.016 c0,2.213,1.351,4.201,3.407,5.017l12.317,4.882v34.925c-2.736,1.865-4.533,5.007-4.533,8.568c0,3.262,1.508,6.171,3.863,8.071 l-3.751,18.007c-0.503,2.416,0.108,4.931,1.666,6.845c1.557,1.915,3.894,3.026,6.361,3.026h4.449c2.468,0,4.804-1.111,6.361-3.026 c1.557-1.914,2.168-4.429,1.666-6.845l-3.752-18.007c2.356-1.9,3.864-4.81,3.864-8.071c0-3.562-1.797-6.703-4.533-8.568v-30.303 l63.729,25.264c5.708,2.263,12.063,2.263,17.771,0l87.709-34.768c2.057-0.815,3.407-2.804,3.407-5.017 C1252,869.886,1250.649,867.897,1248.592,867.082z"></path> </g> </g></svg></span>
-            <div class="pea-timeline-content">
-            <h3 class="pea-title">Stanford University</h3>
-            <h5 class="pea-title-text">Advance Graphic Designer</h5>
-            <p class="pea-description">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit placeat tenetur, maiores, consectetur enim ullam quo earum laborum eius possimus alias repellendus illum!
-            </p>
-            </div>
-        </div>
-
-        <div class="pea-timeline">
-            <span class="pea-icon"><svg  version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  viewBox="1052 796 200 200" enable-background="new 1052 796 200 200" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M1151.998,921.75c-4.129,0-8.17-0.771-12.01-2.292l-50.167-19.888c0,11.08,0,27.65,0,32.066 c0,15.562,27.836,28.174,62.178,28.174s62.181-12.612,62.181-28.174v-32.067l-50.172,19.889 C1160.168,920.979,1156.127,921.75,1151.998,921.75z"></path> <path d="M1248.592,867.082l-87.989-34.878c-5.526-2.19-11.681-2.19-17.208,0l-87.988,34.878c-2.057,0.815-3.407,2.804-3.407,5.016 c0,2.213,1.351,4.201,3.407,5.017l12.317,4.882v34.925c-2.736,1.865-4.533,5.007-4.533,8.568c0,3.262,1.508,6.171,3.863,8.071 l-3.751,18.007c-0.503,2.416,0.108,4.931,1.666,6.845c1.557,1.915,3.894,3.026,6.361,3.026h4.449c2.468,0,4.804-1.111,6.361-3.026 c1.557-1.914,2.168-4.429,1.666-6.845l-3.752-18.007c2.356-1.9,3.864-4.81,3.864-8.071c0-3.562-1.797-6.703-4.533-8.568v-30.303 l63.729,25.264c5.708,2.263,12.063,2.263,17.771,0l87.709-34.768c2.057-0.815,3.407-2.804,3.407-5.017 C1252,869.886,1250.649,867.897,1248.592,867.082z"></path> </g> </g></svg></span>
-            <div class="pea-timeline-content">
-            <h3 class="pea-title">Boston State University</h3>
-            <h5 class="pea-title-text">Visual Art & Design</h5>
-            <p class="pea-description">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit placeat tenetur, maiores, consectetur enim ullam quo earum laborum eius possimus alias repellendus illum!
-            </p>
-            </div>
-        </div>
-
+           </div>
+  
         </div>
 
 	 <?php
